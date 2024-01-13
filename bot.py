c06ff9e688800e6a7f815c5b5c9a54c407352841
Monty0252 <mamuin@buffalo.edu>
2024-01-13 17:35:39 -0500

    first commit

---
+++ bot.py
@@ -0,0 +1,38 @@
+import discord
+import os
+import asyncio
+from discord.ext import commands, tasks
+from itertools import cycle
+import json
+
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+    bot_token = config["DISCORD_BOT_TOKEN"]
+
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+bot_status = cycle(["Chilling", "Chilling More", "Chilling Expeditiously"])
+
+@tasks.loop(seconds=5)
+async def change_status():
+    await bot.change_presence(activity=discord.Game(next(bot_status)))
+
+
+@bot.event
+async def on_ready():
+    print("Bot is connected to Discord Bitchesss")
+    change_status.start()
+
+async def load():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+            print(f"{filename[:-3]} is loaded!")
+
+
+async def main():
+    async with bot:
+        await load()
+        await bot.start(bot_token)
+
+asyncio.run(main())
+
---
+++ cogs/ping.py
@@ -0,0 +1,32 @@
+import discord
+import random
+from discord.ext import commands
+
+
+class Ping(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Ping.py is ready!")
+
+    # Ping Command
+    @commands.command()
+    async def ping(self, ctx):
+        bot_latency = round(self.client.latency * 1000)
+        await ctx.send(f"Pong! {bot_latency} ms.")
+
+    # Magic Eightball Command
+    @commands.command(aliases=['8ball', 'magic8ball'])
+    async def magic_eightball(self, ctx, *, question):
+        with open("TextFiles\eightball_responses.txt", "r") as file:
+            responses = file.readlines()
+
+        response = random.choice(responses).strip()
+        await ctx.send(f"Question: {question}\nAnswer: {response}")
+
+
+
+async def setup(client):
+    await client.add_cog(Ping(client))
